d795732c20bf089d4bcbfd928dc891272c732c23
nakamuratoru <nakamura.toru675@mail.kyutech.jp>
2020-09-09 14:04:03 +0000

    Finish user edit, update, index, and destroy actions

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -26,6 +26,11 @@ module SessionsHelper
     end
   end
   
+  # 渡されたユーザーがカレントユーザーであればtrueを返す
+  def current_user?(user)
+    user && user == current_user
+  end
+  
   def logged_in?
     !current_user.nil?
   end
@@ -43,6 +48,16 @@ module SessionsHelper
     @current_user = nil
   end
   
+  # 記憶したURL（もしくはデフォルト値）にリダイレクト
+  def redirect_back_or(default)
+    redirect_to(session[:forwarding_url] || default)
+    session.delete(:forwarding_url)
+  end
+  
+  # アクセスしようとしたURLを覚えておく
+  def store_location
+    session[:forwarding_url] = request.original_url if request.get?
+  end
   
   
 end
